20f97ee16e585b74b2b58ad9a7dc47f02d1084f3
elsopi <elsopi02@gmail.com>
2022-01-21 03:58:00 +0100

    changed websocket address

--- src/Utils/laravel-echo.js
+++ src/Utils/laravel-echo.js
@@ -6,7 +6,7 @@ export const LEcho = new Echo({
     broadcaster: 'pusher',
     key: process.env.REACT_APP_PUSHER_APP_KEY,
     cluster: process.env.REACT_APP_PUSHER_APP_CLUSTER,
-    wsHost: process.env.NODE_ENV !== 'production' ? window.location.hostname : 'pitch90.spiracsports.com',
+    wsHost: process.env.NODE_ENV !== 'production' ? window.location.hostname : 'www.betteck.com',
     wsPort:            process.env.NODE_ENV !== 'production' ? 6001 : 6002,
     wssPort:           process.env.NODE_ENV !== 'production' ? 6001 : 6002,
     forceTLS: false,
