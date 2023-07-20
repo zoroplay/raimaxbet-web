1d83957b33e70d5275b2ee18ebf4b797e9307fba
Oghenero-Sigbenu <sigbenuoghenero@github.com>
2023-07-20 16:23:12 +0100

    updates

--- src/Views/Account/Deposit.js
+++ src/Views/Account/Deposit.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "../../Assets/scss/_deposit.scss";
+import logo from "../../Assets/img/logo.jpeg";
 import useSWR from "swr";
 import { SHOW_INFO_UPDATE_MODAL } from "../../Redux/types";
 import { useDispatch, useSelector } from "react-redux";
 
-
 export function Deposit({ history }) {
-  const {user} = useSelector(state => state.auth);
-  const {data, error} = useSWR('utilities/payment-methods');
+  const { user } = useSelector((state) => state.auth);
+  const { data, error } = useSWR("utilities/payment-methods");
   const paymentMethods = data?.data || null;
-  const dispatch = useDispatch();  
+  const dispatch = useDispatch();
 
   const confirmDeposit = (provider) => {
-    if (provider !== 'sbengine' && user?.email === null) {
-      dispatch({type: SHOW_INFO_UPDATE_MODAL})
+    if (provider !== "sbengine" && user?.email === null) {
+      dispatch({ type: SHOW_INFO_UPDATE_MODAL });
     } else {
-      history.replace(`/Account/DepositWith/${provider}`)
+      history.replace(`/Account/DepositWith/${provider}`);
     }
-  }
+  };
 
+  console.log(paymentMethods);
   return (
     <>
       <div id="MainContent" className="">
@@ -29,53 +29,74 @@ export function Deposit({ history }) {
               <div className="payments">
                 <div className="RiquadroSrc">
                   <div className="Cnt">
-                  <div className="deposit">
-                    <div className="deposit-table">
-                      <div className="deposit-table-head">
-                        <div className="first">
-                          <h3 className="table-head"></h3>
-                        </div>
-                        <div className="second">
-                          <h4 className="table-head">Payment Method</h4>
-                        </div>
-                        <div className="third">
-                          <h4 className="table-head">Fee</h4>
-                        </div>
-                        <div className="fourth">
-                          <h4 className="table-head">Minimum Amount</h4>
-                        </div>
-                        <div className="fifth">
-                          <h4 className="table-head"></h4>
-                        </div>
-                      </div>
-                      {!paymentMethods ? 
-                        <div className="deposit-table-body" style={{textAlign: 'center', width: '100%', display: 'block'}}>
-                          <h1>Fetching Data...</h1>
-                        </div>
-                      :
-                      paymentMethods?.map((item, index) => (
-                        <div className="deposit-table-body" key={index}>
+                    <div className="deposit">
+                      <div className="deposit-table">
+                        <div className="deposit-table-head">
                           <div className="first">
-                            <img src={`/img/${item?.provider}.png`} alt="logo" />
+                            <h3 className="table-head"></h3>
                           </div>
                           <div className="second">
-                            <h4 className="">{item?.title}</h4>
+                            <h4 className="table-head">Payment Method</h4>
                           </div>
                           <div className="third">
-                            <h4 className="">None</h4>
+                            <h4 className="table-head">Fee</h4>
                           </div>
                           <div className="fourth">
-                            <h4 className="">1000</h4>
+                            <h4 className="table-head">Minimum Amount</h4>
                           </div>
                           <div className="fifth">
-                            <button className="btn">
-                              <a onClick={() => confirmDeposit(item.provider)}>+ Deposit</a>
-                            </button>
+                            <h4 className="table-head"></h4>
                           </div>
                         </div>
-                      ))}
+                        {!paymentMethods ? (
+                          <div
+                            className="deposit-table-body"
+                            style={{
+                              textAlign: "center",
+                              width: "100%",
+                              display: "block",
+                            }}
+                          >
+                            <h1>Fetching Data...</h1>
+                          </div>
+                        ) : (
+                          paymentMethods?.map((item, index) => (
+                            <div className="deposit-table-body" key={index}>
+                              <div className="first">
+                                <img
+                                  src={`${
+                                    item?.provider === "sbengine"
+                                      ? logo
+                                      : `/img/${item?.provider}.png`
+                                  }`}
+                                  alt="logo"
+                                />
+                              </div>
+                              <div className="second">
+                                <h4 className="">{item?.title}</h4>
+                              </div>
+                              <div className="third">
+                                <h4 className="">None</h4>
+                              </div>
+                              <div className="fourth">
+                                <h4 className="">1000</h4>
+                              </div>
+                              <div className="fifth">
+                                <button className="btn">
+                                  <a
+                                    onClick={() =>
+                                      confirmDeposit(item.provider)
+                                    }
+                                  >
+                                    + Deposit
+                                  </a>
+                                </button>
+                              </div>
+                            </div>
+                          ))
+                        )}
+                      </div>
                     </div>
-                  </div>
                   </div>
                 </div>
               </div>
